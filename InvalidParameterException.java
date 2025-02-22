5cd97cc8ed9124f2fffb38b94bfa165b3ecc9b4a
SaraSARA1024 <sara248369@gmail.com>
2025-02-22 20:48:41 +0200

    Add class InvalidParameterException.java

---
+++ InvalidParameterException.java
@@ -0,0 +1,12 @@
+package lesson6.sara.hajbi;
+
+public class InvalidParameterException extends Exception {
+
+    public InvalidParameterException (String message)
+    {
+        super(message);
+    }
+
+
+
+}
